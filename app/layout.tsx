e8abdb9d7f799b47ff50cda79b97ab1e10cc38c1
asnif <asnifkl@gmail.com>
2026-02-06 12:13:58 +0530

    ree

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/layout/Footer";
 import { AuthProvider } from "@/app/context/AuthContext";
 import { usePathname } from "next/navigation"; 
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className="bg-black text-white antialiased overflow-x-hidden">
         <AuthProvider>
+          <Toaster position="top-center" reverseOrder={false} />
           {!isAdminPage && <Navbar />}
           {children}
           {!isAdminPage && <Footer />}
